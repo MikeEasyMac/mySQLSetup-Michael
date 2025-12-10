32d1cd154e08077a16360bfa72b2fbd036eda005
Michael Johnson <mikeeasy11235@gmail.com>
2025-12-10 15:58:14 +0000

    Configured addBook to connect to Cloud SQL and insert my book

--- addBook.js
+++ addBook.js
@@ -1,40 +1,26 @@
-//Author: Sahar Al Seesi
-//Date: 10/17/2020
-//Code tests mysql installation by adding a record to a table book in database 
+const mysql = require('mysql2');
 
-const mysql      = require('mysql2');
-//Create  a connection pool with mySQL server redentials and dataabase name
-const connection_pool = mysql.createPool({
-  host     : '34.73.13.150',
-  user     : 'nodeuser',
-  password : '12345',
-  database : 'testInstallation',
-  connectionLimit : 10
+const connection = mysql.createConnection({
+  host: '34.73.51.247',
+  user: 'nodeuser',
+  password: 'Password123$',
+  database: 'testInstallation'
 });
- 
 
-//Add a record in the database
-//edit this line to add the name of a book you would like to write and your name as the author
-connection_pool.query("INSERT INTO book (title, author) VALUES ('Fall Colors', 'Sahar');", function (error, insert_results, fields) {
-  if (error) {
-	console.log(error);
-	connection_pool.end();
-  }
-	  
-  else {	
-	 // retrieve the whole book table from the database
-	  connection_pool.query('SELECT * FROM book;', function (error, select_results, fields) {
-  	  	if (error) {
-			console.log(error);
-			connection_pool.end();
-		}
-          	else {
-		  	console.log('results ');
-			for (x of select_results) //print the contentes of each records
-				for (y in x)
-					console.log(y+ ":"+x[y]); 
-  		  	connection_pool.end();
-		     } //else
-		}); //end query SELECT
-	} //else
+const book = { title: 'Your Book Title', author: 'Michael Johnson' };
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log('Connected');
+
+  connection.query('INSERT INTO book SET ?', book, function (err, result) {
+    if (err) throw err;
+    console.log('Inserted row id', result.insertId);
+
+    connection.query('SELECT * FROM book', function (err, rows) {
+      if (err) throw err;
+      console.log(rows);
+      connection.end();
+    });
+  });
 });
